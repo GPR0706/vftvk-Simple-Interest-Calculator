1847fb0e657930608bbf471dca744d57b3464c8d
GPR0706 <87071946+GPR0706@users.noreply.github.com>
2021-07-12 00:19:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ function compute() {
 
     document.getElementById("answer1").innerHTML = "If you deposit $" + principal;
     document.getElementById("answer2").innerHTML = "At a rate of " + interestRate + '%';
-    document.getElementById("answer3").innerHTML = "You'll recieve the amount of $" + interestEarned
+    document.getElementById("answer3").innerHTML = "You'll recieve the amount of $" + interestValue
     document.getElementById("answer4").innerHTML = "In the year " + futureDate
 }  
