a04e0f8680aef38229cdbeec2b839f8629e0c0ee
GPR0706 <87071946+GPR0706@users.noreply.github.com>
2021-07-12 01:00:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function compute() {
 
-    var principal = document.getElementById("amountOne").value 
-    var interestRate = document.getElementById("rate").value
-    var years = document.getElementById("noYears").value
-    var earnedInterest = principal * years * interestRate / 100;
-    var n = new Date().getFullYear();
-    var futureDate = Number(n) + Number(years); 
+    let principal = document.getElementById("amountOne").value 
+    let interestRate = document.getElementById("rate").value
+    let years = document.getElementById("noYears").value
+    let interestEarned = principal * years * interestRate / 100;
+    let d = new Date();
+    let n = d.getFullYear();
+    let futureDate = Number(n) + Number(years); 
 
     document.getElementById("answer1").innerHTML = "If you deposit $" + principal;
     document.getElementById("answer2").innerHTML = "At a rate of " + interestRate + '%';
-    document.getElementById("answer3").innerHTML = "You'll recieve the amount of $" + earnedInterest
+    document.getElementById("answer3").innerHTML = "You'll recieve the amount of $" + interestEarned
     document.getElementById("answer4").innerHTML = "In the year " + futureDate
 }   
